659371fb6c6c4f808f78447ce5b027df6e4fce2e
daqnal <dxgd@pm.me>
2023-07-07 20:59:47 -0400

    troubleshooting images not loading on server

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// import { saveAs } from "./FileSaver.js"
-
-// function saveStaticDataToFile(data) {
-//     var blob = new Blob([data],
-//                 { type: "text/plain;charset=utf-8" });
-//     saveAs(blob, "static.txt");
-// }
