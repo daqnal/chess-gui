3db6b7938be378ee9d55ab6c0319f75f76bccbd2
daqnal <dxgd@pm.me>
2023-07-10 10:30:28 -0400

    lol

--- js/game.js
+++ js/game.js
@@ -10,19 +10,6 @@ var $status = $('#status')
 var $fen = $('#fen')
 var $pgn = $('#pgn')
 
-let wPVal = 10;
-let wNVal = 30;
-let wBVal = 30;
-let wRVal = 50;
-let wQVal = 90;
-let wKVal = 900;
-let bPVal = -10;
-let bNVal = -30;
-let bBVal = -30;
-let bRVal = -50;
-let bQVal = -90;
-let bKVal = -900;
-
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
